953c93c7e57446723da4b4fb3c470c6b498e4380
ajaymathe <ajay.mathe9@gmail.com>
2019-01-05 08:56:32 +0530

    binary

---
+++ python_basics/binary_number.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3 
+
+################################################################
+#                                                              #
+# @Author     : Ajay Mathe                                     #
+# @Date       : 2018-12-15 20:18:22                            #
+# @Description: convert decimal to binary                      #
+#                                                              #
+#                                                              #
+#                                                              #
+################################################################
+
+number = 10
+
+print("hey")
+i=0
+
+while number > 0:
+    rem[i] = number%2
+    number = number/2
+    i += 1
+
+print(rem)              
